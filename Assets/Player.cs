e2afafd39d9db0577b54e85fc770467576e2027f
cometores <anicometores@gmail.com>
2024-11-23 21:56:29 +0100

    Character | XY movement, jumping & running

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,15 +1,30 @@
 using System;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class Player : MonoBehaviour
 {
     private Rigidbody2D _rb;
     private Animator _anim;
 
-    [SerializeField] private float moveSpeed;
+    [Header("Movement")] [SerializeField] private float moveSpeed;
+    [SerializeField] private float jumpForce;
 
-    public float xInput;
-    private static readonly int IsRunning = Animator.StringToHash("isRunning");
+    public bool canDoubleJump;
+
+    [Header("Collision info")] [SerializeField]
+    private float groundCheckDistance;
+
+    [SerializeField] private LayerMask whatIsGround;
+    private bool _isGrounded;
+
+    private float _xInput;
+    private bool _facingRight = true;
+    private int _facingDirection = 1;
+
+    private static readonly int XVelocity = Animator.StringToHash("xVelocity");
+    private static readonly int YVelocity = Animator.StringToHash("yVelocity");
+    private static readonly int IsGrounded = Animator.StringToHash("isGrounded");
 
     private void Awake()
     {
@@ -19,13 +34,61 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
-        xInput = Input.GetAxisRaw("Horizontal");
-        _anim.SetBool(IsRunning, _rb.linearVelocity.x != 0);
+        HandleCollision();
+        HandleInput();
         HandleMovement();
+        HandleFlip();
+        HandleAnimation();
+    }
+
+    private void HandleCollision()
+    {
+        _isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, whatIsGround);
+    }
+
+    private void HandleInput()
+    {
+        _xInput = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetKeyDown(KeyCode.Space) && _isGrounded)
+        {
+            Jump();
+        }
+    }
+
+    private void Jump()
+    {
+        _rb.linearVelocityY = jumpForce;
+    }
+
+    private void HandleAnimation()
+    {
+        _anim.SetFloat(XVelocity, _rb.linearVelocityX);
+        _anim.SetFloat(YVelocity, _rb.linearVelocityY);
+        _anim.SetBool(IsGrounded, _isGrounded);
     }
 
     private void HandleMovement()
     {
-        _rb.linearVelocity = new Vector2(xInput * moveSpeed, _rb.linearVelocity.y);
+        _rb.linearVelocityX = _xInput * moveSpeed;
+    }
+
+    private void HandleFlip()
+    {
+        if (_rb.linearVelocityX < 0 && _facingRight || _rb.linearVelocityX > 0 && !_facingRight)
+            FLip();
+    }
+
+    private void FLip()
+    {
+        transform.Rotate(0, 180, 0);
+        _facingRight = !_facingRight;
+        _facingDirection *= -1;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(transform.position,
+            new Vector2(transform.position.x, transform.position.y - groundCheckDistance));
     }
 }
