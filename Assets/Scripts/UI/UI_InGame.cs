7bf394bb8edfb2077d107d1554d9363a18940aad
cometores <anicometores@gmail.com>
2025-03-07 14:54:58 +0100

    UI | Pause menu

--- Assets/Scripts/UI/UI_InGame.cs
+++ Assets/Scripts/UI/UI_InGame.cs
@@ -1,7 +1,7 @@
 using System;
-using System.Globalization;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UI_InGame : MonoBehaviour
 {
@@ -10,9 +10,39 @@ public class UI_InGame : MonoBehaviour
     [SerializeField] private TextMeshProUGUI timerText;
     [SerializeField] private TextMeshProUGUI fruitsText;
 
+    [SerializeField] private GameObject pauseUI;
+    private bool _isPaused;
+    
     private void Awake()
     {
         instance = this;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            PauseButton();
+    }
+
+    public void PauseButton()
+    {
+        if (_isPaused)
+        {
+            _isPaused = false;
+            Time.timeScale = 1f;
+            pauseUI.SetActive(false);
+        }
+        else
+        {
+            _isPaused = true;
+            Time.timeScale = 0;
+            pauseUI.SetActive(true);
+        }
+    }
+
+    public void GoToMainMenuButton()
+    {
+        SceneManager.LoadScene(0);
     }
 
     public void UpdateTimer(float time)
